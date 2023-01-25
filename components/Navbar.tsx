dba4347d680bba042b922f8c659cf7fc03512dde
notsosimppole <gitansh1208@gmail.com>
2023-01-25 13:40:17 +0530

    responsive changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = (props: Props) => {
     <div className={`${props.className}`}>
       <div className="flex justify-evenly w-1/2 relative -left-[5.5rem]">
         <h2 className="text-[#009641]">Home</h2>
-        <h2>About</h2>
+        <h2 className="hover:text-[#009641]">About</h2>
       </div>
       <div className="">
         <img
@@ -21,8 +21,8 @@ const Navbar = (props: Props) => {
         />
       </div>
       <div className="flex justify-evenly w-1/2 relative left-28">
-        <h2>Services</h2>
-        <h2>Contact</h2>
+        <h2 className="hover:text-[#009641]">Services</h2>
+        <h2 className="hover:text-[#009641]">Contact</h2>
       </div>
     </div>
   );
